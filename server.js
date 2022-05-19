49bd3a43f270360fb036039a769ed764eeae26e7
annkim7@github.com <ilc7748@gmail.com>
2022-05-19 18:29:42 +0900

    1-10(session 1 completed)

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended : true }));
 
 app.listen(8080, function(){
     console.log('listening on 8080');
@@ -8,11 +10,23 @@ app.listen(8080, function(){
 //누군가가 /pet으로 방문을 하면
 //pet 관련된 안내문을 띄워주자
 
-app.get('/pet', function(요청, 응답){
-    응답.send('펫용품 쇼핑할 수 있는 페이지입니다');
+// app.get('/pet', function(요청, 응답){
+//     응답.send('펫용품 쇼핑할 수 있는 페이지입니다');
+// });
+
+app.get('/', function(요청, 응답){
+    응답.sendFile(__dirname + '/index.html');
 });
 
-//과제
-app.get('/beauty', function(요청, 응답){
-    응답.send('뷰티용품 쇼핑할 수 있는 페이지입니다');
+app.get('/write', function(요청, 응답){
+    응답.sendFile(__dirname + '/write.html');
+});
+
+//어떤 사람이 /add 경로로 POST 요청을 하면...
+//??를 해주세요...
+
+app.post('/add', function(요청, 응답){
+    응답.send('전송완료');
+    console.log(요청.body);
+    //DB에 저장해주세요
 });
