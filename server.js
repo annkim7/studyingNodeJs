76659e6229274b7907b02573940089220368f70b
annkim7@github.com <ilc7748@gmail.com>
2022-06-03 16:50:43 +0900

    3-10

--- server.js
+++ server.js
@@ -94,11 +94,25 @@ app.get('/list', function(요청, 응답){
 
 app.get('/search', (요청, 응답) => {
     console.log(요청.query.value);
-    db.collection('post').find({제목:/abc/}).toArray((에러, 결과)=>{
+    var 검색조건 = [
+        {
+            $search: {
+              index: 'titleSearch',
+              text: {
+                query: 요청.query.value,
+                path: '제목'  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
+              }
+            }
+          },
+        //   { $sort : { _id: 1 } },
+        //   { $limit : 10 }
+        // { $project : { 제목 : 1, _id : 0, score: { $meta: "searchScore" } } }
+    ];
+    db.collection('post').aggregate(검색조건).toArray((에러, 결과)=>{
         console.log(결과);
         응답.render('search.ejs', { posts : 결과 });
     })
-})
+});
 
 app.delete('/delete', function(요청, 응답){
     console.log(요청.body);
